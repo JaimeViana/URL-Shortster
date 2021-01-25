ff9c13a93e6938d91f2ab2133f82ded72e471329
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-01-25 18:00:25 +0100

    Database connected.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
+
+/**
+ * Connect database
+ */
+connectDB();
 
 app.use(express.json());
 app.use(cors());
